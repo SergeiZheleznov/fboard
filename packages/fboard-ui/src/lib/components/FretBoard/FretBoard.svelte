b39e4a8decc0221fa8c3d1445ff71152ea9288b6
Sergei Zheleznov <sergei.zheleznov@outlook.com>
2022-07-11 23:49:17 +0200

    update dependencies

--- packages/fboard-ui/src/lib/components/FretBoard/FretBoard.svelte
+++ packages/fboard-ui/src/lib/components/FretBoard/FretBoard.svelte
@@ -24,7 +24,7 @@
 	}
 
 	onMount(() => {
-		// $fretBoardWrapper.style.height = $baseWrapper.clientHeight + 'px';
+		$fretBoardWrapper.style.height = $baseWrapper.clientHeight + 'px';
 	});
 </script>
 
@@ -33,8 +33,8 @@
 </div>
 <div bind:this={$fretBoardWrapper} class="fboard-wrapper@(w-full overflow-hidden flex absolute bg-stone-900 top-0 left-0 z-0)">
 	{#each frets as fret}
-		<div style={`width: ${fret.width}%`} class="@(flex h-full)">
-			<div class="@(grow flex items-center justify-center)">
+		<div style={`width: ${fret.width}%`} class="fret@(flex h-full)">
+			<div class="fret_inner@(grow flex items-center justify-center)">
 				{#if [5, 7, 9, 12, 15, 17, 19, 21].includes(fret.idx)}
 					<div class="h-5 w-5 rounded-full bg-stone-300" />
 				{/if}
